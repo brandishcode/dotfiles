2102a226dfe7269939b7fe6cd9746e1fdf6dbbf7
Travis Norman Vallon <travisnormanv@gmail.com>
2024-03-12 20:28:17 +0900

    nvim configuration update
    - Include lua_ls language server

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { 'java' },
+  ensure_installed = { 'java', 'lua', 'c', 'cpp' },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -25,3 +25,4 @@ require'nvim-treesitter.configs'.setup {
     enable_close_on_slash = true
   }
 }
+
