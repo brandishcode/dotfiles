919d176a315c51ad77f15f1a183cd196f9e260af
Travis Norman Vallon <travisnormanv@gmail.com>
2024-03-26 21:22:43 +0900

    nvim configuration update - Updated telescope dependency - Added view diagnostic shortcut - Remove highlight when search - Added tsserver and tailwindcss as lsp server

--- .config/nvim/lua/travisdev/mapping.lua
+++ .config/nvim/lua/travisdev/mapping.lua
@@ -4,4 +4,5 @@ vim.keymap.set('n', '<leader>W', vim.cmd.Lex)
 
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Goto Previous [D]iagnostic' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Goto Next [D]iagnostic' })
+vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, { desc = '[V]iew [D]iagnostic' })
 vim.keymap.set('n', 'nh', vim.cmd.nohl, { desc = 'Remove search highlight' })
--- .config/nvim/lua/travisdev/packer.lua
+++ .config/nvim/lua/travisdev/packer.lua
@@ -32,7 +32,7 @@ return require('packer').startup(function(use)
   use 'mbbill/undotree'
 
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
